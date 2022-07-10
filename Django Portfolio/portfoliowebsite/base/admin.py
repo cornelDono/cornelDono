55bf7f5ab56ea17efef28396d9151278f7eb019e
cornelDono <strenkovsky@gmail.com>
2022-07-10 19:36:45 +0300

    delete

--- Django Portfolio/portfoliowebsite/base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Skill, Tag, Project, PBI_articles
-
-admin.site.register(Project)
-admin.site.register(Skill)
-admin.site.register(Tag)
-admin.site.register(PBI_articles)
--- Django Portfolio/portfoliowebsite/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('', views.homePage, name='home'),
-    path('project/<uuid:pk>', views.projectPage, name='project'),
-    path('pbiProject', views.pbiProjectPage, name='pbiProject'),
-]
